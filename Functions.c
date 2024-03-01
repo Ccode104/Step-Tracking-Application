ee9047d2f29418043a7d14009f472c5fab978ce2
Abhishek Chandurkar <cabhishek1304@gmail.com>
2024-03-01 22:58:35 +0530

    Defined CreateGroup function

--- Functions.c
+++ Functions.c
@@ -1,1 +1,35 @@
-CreateGroup
+#include<stdlib.h>
+#include<stdio.h>
+#include"functions.h"
+
+status_code CreateGroup(NodeG *nptr)
+{
+	NodeG *ptr=lptrG;
+	NodeG *prev=NULL;
+	status_code sc=SUCCESS;
+
+	while(ptr->Id>nptr->Id)
+	{
+		prev=ptr;
+		ptr=ptr->next;
+	}
+
+	if(ptr->Id==nptr->Id)
+	{
+		sc=FAILURE;
+	}
+	else{
+		if(prev==NULL)
+		{
+			//Insert At Start
+			nptr->next=lptrG;
+			lptrG=nptr;
+		}
+		else{
+			nptr->next=ptr;
+			prev->next=nptr;
+		}
+	}
+	return sc;
+
+}
